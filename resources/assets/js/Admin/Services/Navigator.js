507edac37d0a392deddac6b6679b93ff0d02f740
Krišjānis <1071453+rerzy@users.noreply.github.com>
2019-10-29 11:28:05 +0200

    Updated navigator logic & include it in primary admin module

--- resources/assets/js/Admin/Services/Navigator.js
+++
@@ -1,8 +0,0 @@
-export default class Navigator {
-    constructor() {
-        this.registerEventListeners();
-    }
-
-    registerEventListeners() {
-    }
-}
--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,4 +1,5 @@
 import FieldRegistry from "./Admin/FieldRegistry";
 import AdminPanel from "./Admin/AdminPanel";
+import Navigator from './Admin/Navigator/Navigator'
 
-export default new AdminPanel(FieldRegistry);
+export default new AdminPanel(FieldRegistry, new Navigator());
