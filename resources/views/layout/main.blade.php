2b487fb8920ca52a4850f6912146ef340c7b58ab
Krišjānis <1071453+rerzy@users.noreply.github.com>
2020-03-03 16:58:04 +0200

    Use newer font awesome version

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" href="/vendor/arbory/images/favicon.ico">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         @include('arbory::layout.partials.assets-css')
 
